bb4eef6375284e05655f941cac225fb7f0358adf
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-27 14:30:56 -0600

    Update page.js

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,14 +1,14 @@
 import ReservationList from "@/app/_components/ReservationList";
-// import { auth } from "@/app/_lib/auth";
-// import { getBookings } from "@/app/_lib/data-service";
+import { auth } from "@/app/_lib/auth";
+import { getBookings } from "@/app/_lib/data-service";
 
 export const metadata = {
   title: "Reservations",
 };
 
 export default async function Page() {
-  //   const session = await auth();
-  //   const bookings = await getBookings(session.user.guestId);
+  const session = await auth();
+  const bookings = await getBookings(session.user.guestId);
 
   return (
     <div>
@@ -16,7 +16,7 @@ export default async function Page() {
         Your reservations
       </h2>
 
-      {/* {bookings.length === 0 ? (
+      {bookings.length === 0 ? (
         <p className="text-lg">
           You have no reservations yet. Check out our{" "}
           <a className="underline text-accent-500" href="/cabins">
@@ -25,7 +25,7 @@ export default async function Page() {
         </p>
       ) : (
         <ReservationList bookings={bookings} />
-      )} */}
+      )}
     </div>
   );
 }
