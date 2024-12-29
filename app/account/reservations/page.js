e5527e71ca9bfc819a883fc7b892362a29828a93
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-29 10:14:05 -0600

    Update page.js

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -9,7 +9,8 @@ export const metadata = {
 
 export default async function Page() {
   const session = await auth();
-  const bookings = await getBookings(session.user.guestId);
+
+  const bookings = await getBookings(session?.user?.guestId);
 
   return (
     <div>
@@ -17,7 +18,9 @@ export default async function Page() {
         Your reservations
       </h2>
 
-      {bookings.length === 0 ? (
+      {!session ? (
+        "Please sign in"
+      ) : !bookings || bookings?.length === 0 ? (
         <p className="text-lg">
           You have no reservations yet. Check out our{" "}
           <Link className="underline text-accent-500" href="/cabins">
