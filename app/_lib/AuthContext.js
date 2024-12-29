ef835aa64724e4eba679cafa21b8eaebf70a4b2c
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-29 11:10:08 -0600

    Create AuthContext.js

---
+++ app/_lib/AuthContext.js
@@ -0,0 +1,33 @@
+"use client";
+
+import { createContext, useContext, useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+
+// A change from Jonas code. Created GuestLink and AuthContext, then wrapped the app's html in layout
+// inside AuthProvider and SessionProvider to make it possible to still show the avatar or whatever
+// session info in the Navigation component, without making the whole app dynamic.
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const { data: session, status } = useSession();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (status === "loading") {
+      setLoading(true);
+    } else {
+      setUser(session?.user || null);
+      setLoading(false);
+    }
+  }, [session, status]);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
