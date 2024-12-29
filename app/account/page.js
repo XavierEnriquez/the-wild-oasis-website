568686c2f5c45bc49b94bb7a691664feb85e2ae9
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-29 10:14:18 -0600

    Update page.js

--- app/account/page.js
+++ app/account/page.js
@@ -7,11 +7,11 @@ export const metadata = {
 export default async function Page() {
   const session = await auth();
 
-  const firstName = session.user.name.split(" ").at(0);
+  const firstName = session?.user?.name.split(" ").at(0);
 
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, {firstName}
+      {session ? ` Welcome, ${firstName}` : "Welcome. Please sign in"}
     </h2>
   );
 }
