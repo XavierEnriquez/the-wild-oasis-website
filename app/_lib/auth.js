3b965066a2acc938a4feacf67092b85ece73f692
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-30 11:11:57 -0600

    Update auth.js

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -2,38 +2,47 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { createGuest, getGuest } from "./data-service";
 
-// Google({
+// nextjs 15 auto infers this code form the passed providers
+// providers: [Google({
 //   clientId: process.env.AUTH_GOOGLE_ID,
 //   clientSecret: process.env.AUTH_GOOGLE_SECRET,
-// }),
+// }),]
 
 const authConfig = {
   providers: [Google],
-  // callbacks: {
-  //   authorized({ auth, request }) {
-  //     return !!auth?.user;
-  //   },
-  //   async signIn({ user, account, profile }) {
-  //     try {
-  //       const existingGuest = await getGuest(user.email);
+  callbacks: {
+    authorized({ auth, request }) {
+      // !! boolean (true, false) operator
+      // return true if user, false otherwise
+      return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
 
-  //       if (!existingGuest)
-  //         await createGuest({ email: user.email, fullName: user.name });
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
 
-  //       return true;
-  //     } catch {
-  //       return false;
-  //     }
-  //   },
-  //   async session({ session, user }) {
-  //     const guest = await getGuest(session.user.email);
-  //     session.user.guestId = guest.id;
-  //     return session;
-  //   },
-  // },
-  // pages: {
-  //   signIn: "/login",
-  // },
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    // session is a nextjs function
+    async session({ session, user }) {
+      // get guest from supabase
+      const guest = await getGuest(session.user.email);
+
+      // mutate the current session and add guest id
+      session.user.guestId = guest.id;
+
+      // return back the session with user id included
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
 };
 
 export const {
