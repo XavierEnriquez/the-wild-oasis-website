bb99d95af3d5f7577bf190a1e5cd1d90cbb4e886
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-01-03 14:23:28 -0600

    Update ReservationForm.js

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -28,7 +28,7 @@ function ReservationForm({ cabin, user }) {
 
   return (
     <div className="h-auto">
-      <div className="bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center">
+      <div className="bg-primary-800 text-primary-300 px-4 md:px-10 lg:px-14 py-2 flex justify-between md:justify-end gap-8 items-center">
         <p>Logged in as</p>
 
         <div className="flex gap-4 items-center">
@@ -52,7 +52,7 @@ function ReservationForm({ cabin, user }) {
           await createBookingWithData(formData);
           resetRange();
         }}
-        className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col"
+        className="bg-primary-900 py-8 px-4 md:px-10 lg:px-14 text-lg flex gap-5 flex-col"
       >
         <div className="space-y-2">
           <label htmlFor="numGuest">How many guests?</label>
