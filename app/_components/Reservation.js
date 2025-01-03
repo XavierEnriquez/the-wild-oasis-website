031ae3f8a7d500a64d447d9f09c8b5a43c6cd2f7
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-01-03 14:24:30 -0600

    Update Reservation.js

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -24,18 +24,6 @@ async function Reservation({ cabin }) {
         <LoginMessage />
       )}
     </div>
-    // <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
-    //   <DateSelector
-    //     settings={settings}
-    //     bookedDates={bookedDates}
-    //     cabin={cabin}
-    //   />
-    //   {session?.user ? (
-    //     <ReservationForm cabin={cabin} user={session.user} />
-    //   ) : (
-    //     <LoginMessage />
-    //   )}
-    // </div>
   );
 }
 
