5ea791ff507a8d20df0e5e298ec947215d12ba68
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-01-02 22:51:41 -0600

    Update Logo.js

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -12,7 +12,7 @@ function Logo() {
         quality={100}
         alt="The Wild Oasis logo"
       />
-      <span className="text-xl font-semibold text-primary-100">
+      <span className="hidden sm:block text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
     </Link>
