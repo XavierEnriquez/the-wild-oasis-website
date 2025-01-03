5f051041f05f5bd2b3da0a784f080b73a1a4d21a
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-01-03 13:16:18 -0600

    Update layout.js

--- app/account/layout.js
+++ app/account/layout.js
@@ -2,7 +2,7 @@ import SideNavigation from "@/app/_components/SideNavigation";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex h-full gap-4 lg:gap-12">
+    <div className="flex flex-col md:flex-row h-full gap-6 lg:gap-12">
       <SideNavigation />
       <div className="py-1">{children}</div>
     </div>
