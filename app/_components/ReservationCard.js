5da387a5c2178c447e185ad876552453d96afa1a
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-01-03 13:16:35 -0600

    Update ReservationCard.js

--- app/_components/ReservationCard.js
+++ app/_components/ReservationCard.js
@@ -24,8 +24,8 @@ function ReservationCard({ booking, onDelete }) {
   } = booking;
 
   return (
-    <div className="flex border border-primary-800">
-      <div className="relative h-32 aspect-square">
+    <div className="flex flex-col gap-4 md:flex-row md:gap-0 border border-primary-800">
+      <div className="relative md:h-32 aspect-square">
         <Image
           className="object-cover border-r border-primary-800"
           src={image}
@@ -34,7 +34,7 @@ function ReservationCard({ booking, onDelete }) {
         />
       </div>
 
-      <div className="flex-grow px-6 py-3 flex flex-col">
+      <div className="flex-grow px-6 py-3 flex flex-col gap-2">
         <div className="flex items-center justify-between">
           <h3 className="text-xl font-semibold">
             {numNights} nights in Cabin {name}
@@ -58,7 +58,7 @@ function ReservationCard({ booking, onDelete }) {
           ) &mdash; {format(new Date(endDate), "EEE, MMM dd yyyy")}
         </p>
 
-        <div className="flex gap-5 mt-auto items-baseline">
+        <div className="flex flex-wrap gap-5 mt-auto items-baseline">
           <p className="text-xl font-semibold text-accent-400">${totalPrice}</p>
           <p className="text-primary-300">&bull;</p>
           <p className="text-lg text-primary-300">
@@ -70,12 +70,12 @@ function ReservationCard({ booking, onDelete }) {
         </div>
       </div>
 
-      <div className="flex flex-col border-l border-primary-800 w-[100px]">
+      <div className="flex md:flex-col w-full border-l border-primary-800 md:w-[100px] ">
         {!isPast(startDate) ? (
           <>
             <Link
               href={`/account/reservations/edit/${id}`}
-              className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 border-b border-primary-800 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
+              className="group flex items-center justify-center md:justify-start gap-2 uppercase text-xs font-bold text-primary-300 border-b border-primary-800 flex-grow pb-2 md:pb-0 px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
             >
               <PencilSquareIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
               <span className="mt-1">Edit</span>
