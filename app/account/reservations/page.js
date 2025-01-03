1b31eed721e6fbf67931587f9bd564a5581cc1af
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-01-02 22:51:10 -0600

    Update page.js

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -13,7 +13,7 @@ export default async function Page() {
   const bookings = await getBookings(session?.user?.guestId);
 
   return (
-    <div>
+    <div className=" overflow-y-auto">
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
         Your reservations
       </h2>
