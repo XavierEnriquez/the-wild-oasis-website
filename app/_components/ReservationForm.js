e59512c70b2226770a1670ac9e61fd573c6bb2b2
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-01-02 22:51:34 -0600

    Update ReservationForm.js

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -27,7 +27,7 @@ function ReservationForm({ cabin, user }) {
   const createBookingWithData = createBooking.bind(null, bookingData);
 
   return (
-    <div className="scale-[1.01]">
+    <div className="h-auto">
       <div className="bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center">
         <p>Logged in as</p>
 
@@ -87,7 +87,7 @@ function ReservationForm({ cabin, user }) {
 
         <div className="flex justify-end items-center gap-6">
           {!(startDate && endDate) ? (
-            <p className="text-primary-300 text-base">
+            <p className="text-primary-300 text-base py-4">
               Start by selecting dates
             </p>
           ) : (
