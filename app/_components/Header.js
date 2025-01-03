89b4cc040bb15a690ad8f78ce9b695d0e5cd2f7e
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-01-02 22:51:44 -0600

    Update Header.js

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -1,10 +1,18 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Navigation from "@/app/_components/Navigation";
 import Logo from "@/app/_components/Logo";
 
 function Header() {
+  const pathname = usePathname();
+
+  const bgHome =
+    pathname !== "/" ? "bg-primary-950 border-b border-primary-900" : "";
+
   return (
-    <header className="border-b border-primary-900 px-8 py-5">
-      <div className="flex justify-between items-center max-w-7xl mx-auto">
+    <header className={`${bgHome} lg:sticky lg:z-50 top-0 px-8 py-5`}>
+      <div className="flex justify-between items-center max-w-[100rem] mx-auto">
         <Logo />
         <Navigation />
       </div>
