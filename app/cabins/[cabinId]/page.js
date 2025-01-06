7595c3783e97e9299bc1232408ac30b4530ddf68
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-01-06 11:13:06 -0600

    Update page.js

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -2,8 +2,6 @@ import Cabin from "@/app/_components/Cabin";
 import Reservation from "@/app/_components/Reservation";
 import Spinner from "@/app/_components/Spinner";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
-
-// import Image from "next/image";
 import { Suspense } from "react";
 
 // export const metadata = {
