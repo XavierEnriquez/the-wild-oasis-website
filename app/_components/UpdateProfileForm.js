e689e99c3b511ee8d8756a176f2373865dec54d5
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-01-06 11:12:22 -0600

    Update UpdateProfileForm.js

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 // import { useState } from "react";
 // import Image from "next/image";
 import { updateGuest } from "../_lib/actions";
@@ -39,11 +40,9 @@ function UpdateProfileForm({ guest, children }) {
         <div className="flex items-center justify-between">
           <label htmlFor="nationality">Where are you from?</label>
           {countryFlag && (
-            <img
-              src={countryFlag}
-              alt="Country flag"
-              className="h-5 rounded-sm"
-            />
+            <div className="relative rounded-sm h-5 w-9 ">
+              <Image src={countryFlag} alt="Country flag" className="" fill />
+            </div>
           )}
         </div>
 
