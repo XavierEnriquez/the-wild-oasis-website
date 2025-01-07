d146427f51f79dcd9b4073e2e946a0bb08ba5886
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2025-01-07 14:50:40 -0600

    Update SubmitButton.js

--- app/_components/SubmitButton.js
+++ app/_components/SubmitButton.js
@@ -8,7 +8,7 @@ export default function SubmitButton({ children, pendingLabel }) {
 
   return (
     <button
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold rounded-lg hover:text-primary-100 hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
       disabled={pending}
     >
       {pending ? pendingLabel : children}
